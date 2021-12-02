a1c056e0ec2d9d270baec20a9d59fb323f028907
lizunlong <lizunlong@gmail.com>
2021-12-02 11:59:13 +0800

    serve static files

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const fs = require("fs-extra");
 const express = require("express");
 const cors = require("cors");
@@ -6,16 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/", (_, res) => {
-  res.status(200).json({ ok: true });
-});
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 app.get("/photo", async (req, res) => {
   try {
     const paths = await fs.readdir("./assets/todo");
     const path = paths?.[0];
     if (path) {
-      await fs.ensureFile(`./assets/todo/${path}`)
+      await fs.ensureFile(`./assets/todo/${path}`);
       await fs.move(`./assets/todo/${path}`, `./assets/doing/${path}`);
     }
     res.status(200).json({ ok: true, path });
@@ -24,4 +23,8 @@ app.get("/photo", async (req, res) => {
   }
 });
 
-app.listen(8000);
+app.use("/", (_, res) => {
+  res.status(200).json({ ok: true });
+});
+
+app.listen(8000, () => console.log("server is up!"));
