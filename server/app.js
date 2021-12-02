78234e99c9e2f250568d655d0247fdbf97a715af
lizunlong <lizunlong@gmail.com>
2021-12-02 11:30:02 +0800

    get photo

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const fs = require("fs-extra");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -5,8 +6,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/", (req, res) => {
+app.use("/", (_, res) => {
   res.status(200).json({ ok: true });
+});
+
+app.get("/photo", async (req, res) => {
+  try {
+    const paths = await fs.readdir("./assets/todo");
+    const path = paths?.[0];
+    if (path) {
+      await fs.ensureFile(`./assets/todo/${path}`)
+      await fs.move(`./assets/todo/${path}`, `./assets/doing/${path}`);
+    }
+    res.status(200).json({ ok: true, path });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 app.listen(8000);
